28a8997dc712ba2bc143f74362ff9807995e86a9
PavelPal-lab <legopavel12@gmail.com>
2024-06-25 14:21:47 +0000

    Ark

--- __pycache__/lab_3_task_2.py
+++ __pycache__/lab_3_task_2.py
@@ -1,8 +1,8 @@
 import numpy as np
 from lab_3_task_1 import g
 h = 100
-a = np.pi / 3
-B = 30 / 180 * np.pi
+a = 45 / 180 * np.pi
+B = 35 / 180 * np.pi
 
 v = np.sqrt ((g * h * np.tan(B) ** 2) / (2 * np.cos(a) ** 2 * (1 - np.tan(B) * np.tan(a))))
 print(v)
