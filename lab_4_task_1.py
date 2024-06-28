49d8bbbdcc6881d0fba315957c0466ff727e3556
PavelPal-lab <legopavel12@gmail.com>
2024-06-28 12:34:12 +0000

    Bread

--- lab_4_task_1.py
+++ lab_4_task_1.py
@@ -1,1 +1,13 @@
+import numpy as np
 
+first = int(input("первое число: "))
+second = int(input("второе число: "))
+
+lists = [first, second]
+array = np.array(lists)
+
+def average_value (array):
+    av_va = (array[0] + array[1]) / 2
+    return av_va
+
+print(average_value(array))
