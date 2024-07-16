ec15fff564d7dcc1f0c8001eab68b10c260d92d7
PavelPal-lab <legopavel12@gmail.com>
2024-07-16 08:11:51 +0000

    .

--- lab_6_task_3.py
+++ lab_6_task_3.py
@@ -1,11 +1,26 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def elipce(minimum, maximum, N, a, b):
-  
-  x = (minimum, maximum, N)
-  y = (minimum, maximum, N)
-
-  X, Y = np.meshgrid(x, y)
-
+def plot_ellipse(x_min, x_max, N, a, b):
+  x = np.linspace(x_min, x_max, N)
+  y_positive = np.sqrt(b*2 * (1 - (x*2 / a*2)))
+  y_negative = -np.sqrt(b*2 * (1 - (x*2 / a*2)))
+  plt.plot(x, y_positive, 'r-', label='Верхняя половина')
+  plt.plot(x, y_negative, 'r-', label='Нижняя половина')
+  plt.xlim(x_min, x_max)
+  plt.ylim(-b, b)
+  plt.xlabel('X')
+  plt.ylabel('Y')
+  plt.title('График эллипса')
+  plt.legend()
+  plt.grid(True)
+  plt.axis('equal')
   plt.savefig('fig_task_3.png')
+
+N = int(input("Введите количество точек (N): "))
+x_min = float(input("Введите минимальное значение X: "))
+x_max = float(input("Введите максимальное значение X: "))
+a = float(input("Введите большую полуось (a): "))
+b = float(input("Введите малую полуось (b): "))
+
+plot_ellipse(x_min, x_max, N, a, b)
