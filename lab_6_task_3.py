308b333b00e9cc405355c68f47a543b9d2b57473
PavelPal-lab <legopavel12@gmail.com>
2024-07-16 14:00:19 +0000

    бобик

--- lab_6_task_3.py
+++ lab_6_task_3.py
@@ -1,22 +1,19 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plot_ellipse(a, b, x_min, x_max, N):
-  x = np.linspace(x_min, x_max, N)
-  y_top = np.sqrt(b*2 * (1 - (x*2 / a*2)))
-  y_bottom = -y_top
-  plt.plot(x, y_top, 'r-', label='Верхняя половина')
-  plt.plot(x, y_bottom, 'r-', label='Нижняя половина')
-  plt.xlim(x_min, x_max)
-  plt.ylim(-b, b)
+def ellipse(a, b, x_min, x_max, N):
+  t = np.linspace (0, 2*np.pi, N)
+  x = a * np.cos(t)
+  y = b * np.sin(t)
+
+  plt.plot(x, y)
+
   plt.xlabel('X')
   plt.ylabel('Y')
   plt.title('График эллипса')
-  plt.legend()
   plt.grid(True)
   plt.axis('equal')
   plt.savefig('fig_task_3.png')
-  plt.show()
 
 a = float(input("Введите большую полуось (a): "))
 b = float(input("Введите малую полуось (b): "))
@@ -24,4 +21,4 @@ x_min = float(input("Введите минимальное значение X: "
 x_max = float(input("Введите максимальное значение X: "))
 N = int(input("Введите количество точек (N): "))
 
-plot_ellipse(a, b, x_min, x_max, N)
+ellipse(a, b, x_min, x_max, N)
